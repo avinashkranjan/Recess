69759b72f4d2000ff582a03bb5a944ca35be0675
Sristi27 <sristi2705@gmail.com>
2021-03-17 20:53:53 +0530

    changed ui of form

--- src/ImageUpload.js
+++ src/ImageUpload.js
@@ -1,22 +1,68 @@
 import React, { useState } from "react";
-import { Button, Input } from "@material-ui/core";
+import { Button, Input,Card, CardContent, Typography,CardMedia,CardActions,CircularProgress} from "@material-ui/core";
 import { storage, db } from "./firebase";
 import firebase from "firebase";
+
+import PhotoCamera from '@material-ui/icons/PhotoCamera';
 import "./ImageUpload.css";
 
+import { makeStyles } from '@material-ui/core/styles';
+
+
+
+const useStyles = makeStyles({
+  root: {
+      width:750,
+      margin: '40px auto',
+  },
+  pos: {
+      marginBottom: 12,
+  },
+  action:
+  {
+      width:109,
+      margin:"10px auto"
+  },
+  media: {
+      height: 0,
+      paddingTop: '56.25%',
+    },
+
+  inp:{ 
+      width: "90%", 
+      margin: "10px auto", 
+      padding: "10px" 
+  }
+
+});
+
+
 function ImageUpload({ username }) {
+
+
   const [image, setImage] = useState(null);
   // const [url, setUrl] = useState("");
+  const [upload,setUpload]=useState(false);
   const [progress, setProgress] = useState(0);
   const [caption, setCaption] = useState("");
+  const [file,setFile]=useState(null);
+  
+
+ const classes=useStyles();
+  
 
   const handleChange = (e) => {
     if (e.target.files[0]) {
       setImage(e.target.files[0]);
+      setFile(URL.createObjectURL(e.target.files[0]));
     }
   };
 
+
   const handleUpload = () => {
+
+    setUpload(true);
+    console.log(image)
     const uploadTask = storage.ref(`images/${image.name}`).put(image);
 
     uploadTask.on(
@@ -48,24 +94,87 @@ function ImageUpload({ username }) {
             setProgress(0);
             setCaption("");
             setImage(null);
+            setUpload(false);
+            setFile(null);
           });
       }
     );
   };
 
   return (
-    <div className="imageupload">
-      <progress className="imageupload__progress" value={progress} max="100" />
-      <Input
+
+    <Card className="imageupload">
+      <CardContent>
+        <Typography variant="h5" style={{textAlign:'center',marginBottom:'10px'}}>Add Post</Typography>
+        <form>
+        <Input
         type="text"
         placeholder="Enter a caption.."
         onChange={(e) => setCaption(e.target.value)}
         value={caption}
       />
-      <Input type="file" onChange={handleChange} />
-      <Button onClick={handleUpload}>Upload</Button>
-    </div>
+      <br/>
+        <Input style={{display:"none"}}
+                        id="contained-button-file"
+                        multiple 
+                        onChange={handleChange}
+                        type="file"
+                    /> 
+                    <label htmlFor="contained-button-file">
+                        {!image && !file?
+                        <Button variant="contained" color="primary" 
+                        component="span" startIcon={<PhotoCamera />}
+                        color="primary">
+                            Upload
+                        </Button>:<Button variant="contained" color="primary"
+                        component="span" startIcon={<PhotoCamera />}
+                        color="primary">
+                            Change
+                        </Button>}
+
+                        <br/>
+
+                        {file?<Card style={{width:'80%',margin:"0 auto"}}>
+                            <CardMedia
+                            className={classes.media}
+                            image={file}/>
+                          </Card>:''}
+                    </label>
+                    
+                </form>
+
+
+                {upload?<progress className="imageupload__progress" value={progress} max="100" />:''}
+
+
+            </CardContent>
+            <CardActions className={classes.action}>
+                <Button type="submit"
+                    style={{ textTransform: "capitalize", fontSize: "17px"}}
+                    variant="contained" color="primary"
+                    onClick={handleUpload}>
+                        Add Post</Button>
+            </CardActions>
+            
+        </Card>
+
+    // <div className="imageupload">
+    //   {uplaod?<progress className="imageupload__progress" value={progress} max="100" />:""}
+    //   
+    //   <Input type="file" onChange={handleChange} />
+    //   <Button variant="container" color="primary">Upload Image</Button>
+    //   <Button onClick={handleUpload}>Add Post</Button>
+    // </div>
   );
 }
 
 export default ImageUpload;
+
+
+
+// <Card className={classes.root}>
+//             <CardContent>
+//                 <Typography variant="h5" 
+//                 style={{ textTransform: "uppercase", textAlign:"center", fontWeight: "bold" }}>
+//                     New Post</Typography>
+                
