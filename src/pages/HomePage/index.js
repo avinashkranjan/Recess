89446b78f3817bded88f462f4f9c430c61aad0a4
Akshay R R <65683151+akshay1027@users.noreply.github.com>
2021-04-14 21:11:07 +0530

    added popup message, will time out after 5 secs!

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,17 +1,41 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
+import { Snackbar, SnackbarContent } from '@material-ui/core';
+
 
 import Post from "../../components/Post";
 
 function Homepage({ posts, user }) {
   const history = useHistory();
+  const [open, setOpen] = useState(true);
+
+  useEffect(() =>{
+    setTimeout(()=>{
+     setOpen(false)
+    }, 5000)
+
+  }, [])
 
   useEffect(() => {
     if (history.location.pathname === "/") history.replace("/home");
   }, []);
 
   return (
-    <>
+    <> 
+      <Snackbar 
+        open={open}
+        anchorOrigin={{
+            vertical: 'center',
+            horizontal: 'top',
+          }}
+          //message="Login or Signup to post and comment!"
+      >
+      <SnackbarContent style={{
+            backgroundColor:'teal',
+            }}
+            message={<h2>Login or Signup to post and comment!</h2>}
+      />
+     </Snackbar>
       {posts.map(({ id, post }) => (
         <Post
           key={id}
